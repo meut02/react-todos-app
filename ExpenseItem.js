423fb0e9dc2579514455d5a6b7f3772048871576
meut02 <136924502+meut02@users.noreply.github.com>
2023-08-01 08:52:49 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,12 @@
+import ExpenseDetail from "./ExpenseDetail";
+
+function ExpenseItem(){
+   return (
+    <div>
+        <h2>Expense Item!!</h2>
+        <ExpenseDetail></ExpenseDetail>
+        </div>
+    
+    );
+        }
+export default ExpenseItem;
